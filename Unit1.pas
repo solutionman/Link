4c89c7a995629f5993bc416383c0a500901e6ce0
solutionman <solutionman@inbox.ru>
2023-01-28 10:47:35 +0700

    'Get'

--- Unit1.pas
+++ Unit1.pas
@@ -4,14 +4,19 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, ShellAPI, StdCtrls;
+  Dialogs, ShellAPI, StdCtrls, HTTPApp, IdBaseComponent, IdComponent,
+  IdTCPConnection, IdTCPClient, IdHTTP;
 
 type
   TForm1 = class(TForm)
     Label1: TLabel;
     Label2: TLabel;
+    Memo1: TMemo;
+    Get: TButton;
+    IdHTTP1: TIdHTTP;
     procedure Label1Click(Sender: TObject);
     procedure Label2Click(Sender: TObject);
+    procedure GetClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -33,6 +38,11 @@ end;
 procedure TForm1.Label2Click(Sender: TObject);
 begin
   ShellExecute(Handle, 'open', 'https://www.youtube.com/', nil, nil, SW_SHOW);
+end;
+
+procedure TForm1.GetClick(Sender: TObject);
+begin
+  Memo1.Lines.Add(IdHTTP1.Get('http://relaxnvacation.ml/') );
 end;
 
 end.
